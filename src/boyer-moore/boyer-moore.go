6ab8d9362c24b3e728f8191605027b0bf9ce2ead
Daniel Ryan <daniel.ryan@students.itb.ac.id>
2018-04-13 19:15:48 +0700

    Add: complete boyer-moore

--- src/boyer-moore/boyer-moore.go
+++ src/boyer-moore/boyer-moore.go
@@ -3,15 +3,18 @@ package main
 import (
 	"fmt"
 	"strings"
+	"bufio"
+	"os"
 )
 
 const CHAR_NUMBER int = 128;
+const NOTHING int = -1;
 
 func analyzeLastOccurence(keywords string) [CHAR_NUMBER]int {
 	var charIndices [CHAR_NUMBER] int;
 
 	for i := 0; i < CHAR_NUMBER; i++ {
-		charIndices[i] = -1;
+		charIndices[i] = NOTHING;
 	}
 	for i := 0; i < len(keywords); i++ {
 		ascii := keywords[i];
@@ -21,7 +24,7 @@ func analyzeLastOccurence(keywords string) [CHAR_NUMBER]int {
 	return charIndices;
 }
 
-func solve(keywords, text string) bool {
+func solve(keywords, text string) int {
 	keywords = strings.ToLower(keywords);
 	text = strings.ToLower(text);
 
@@ -31,21 +34,24 @@ func solve(keywords, text string) bool {
 	iMax := len(text);
 	i := jMax;
 	j := 0;
-	found := false;
+	found := -1;
 
-	for (i < iMax && !found) {
-		for (j <= jMax && keywords[jMax-j] == text[i-j]) {
+	for (i < iMax && found == -1) {
+		for (j <= jMax && keywords[jMax - j] == text[i - j]) {
 			j++;
 		}
 		if (j > jMax) {
-			found = true;
+			found = i - (j - 1);
 		} else {
-			differentChar := text[i - j];
-			differentIndex := jMax - j;
-			if (charIndices[differentChar] > differentIndex) {
-				// Shift i once to right
+			indexInKeywords := jMax - j;
+			indexInText := i - j;
+			charInText := text[indexInText];
+			if (charIndices[charInText] == NOTHING) {
+				i += indexInKeywords + 1;
+			} else if (charIndices[charInText] > indexInKeywords) {
+				i++;
 			} else {
-				// Align the index of the same chara
+				i += indexInKeywords - charIndices[charInText];
 			}
 		}
 	}
@@ -54,9 +60,20 @@ func solve(keywords, text string) bool {
 }
 
 func main() {
-	if (solve("keywords", "text")) {
+	reader := bufio.NewReader(os.Stdin);
+	fmt.Print("Text: ");
+	text, _ := reader.ReadString('\n');
+	fmt.Print("Keywords: ");
+	keywords, _ := reader.ReadString('\n');
+
+	text = text[0:len(text)-1];
+	keywords = keywords[0:len(keywords)-1];
+
+	idx := solve(keywords, text);
+	if (idx != -1) {
 		fmt.Println("found it!");
+		fmt.Println(idx);
 	} else {
-		fmt.Println("cannot found it!");
+		fmt.Println("cannot find it!");
 	}
 }
