2bcd2a934e0cf1a6f21d293fb9f5e732d68c49d7
Louis Leslie <spy.tech23@gmail.com>
2018-04-22 17:49:00 +0700

    Added view

--- index.php
+++ index.php
@@ -1,9 +1,29 @@
+<html>
+	<head>
+		<title>Twitter Spam Detector App</title>
+		<meta charset="utf-8" name="viewport" content="width=device-width, initial-scale=1"/>
+        <link rel="stylesheet" href="view/static/css/bootstrap.min.css">
+        <link rel="stylesheet" href="view/static/css/style.css">
+        <meta >
+	</head>
+	<body>
+		<!-- Content -->
+		<?php
+			include_once "view/templates/layout.html";
+		?>
+
+		<!-- Javascript -->
+        <!-- Placed at the end of document so the pages load faster -->
+        <script type="text/javascript" src="view/static/js/jquery.min.js"></script>
+        <script type="text/javascript" src="view/static/js/script.js"></script>
+        <script type="text/javascript" src="view/static/js/bootstrap.min.js"></script>
+	</body>
+</html>
 <?php
-	header('Content-Type: application/json');
-
-	include_once "scripts/oauth.php";
-	include_once "scripts/utils.php";
-	include_once "scripts/request.php";
-
-	echo json_encode(json_decode(getTimeLine()), JSON_PRETTY_PRINT);
+	// // header('Content-Type: application/json');
+	// include_once "scripts/oauth.php";
+	// include_once "scripts/utils.php";
+	// include_once "scripts/request.php";
+	// include_once "view/main.html";
+	// echo json_encode(json_decode(getTimeLine()), JSON_PRETTY_PRINT);
 ?>
