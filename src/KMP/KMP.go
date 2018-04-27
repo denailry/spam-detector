cc870889d04ac5a0b6a1dd16a9433996154d5927
Louis Leslie <spy.tech23@gmail.com>
2018-04-27 11:46:03 +0700

    Fixed KMP case-sensitivity

--- src/KMP/KMP.go
+++ src/KMP/KMP.go
@@ -1,19 +1,31 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"io/ioutil"
-	"encoding/json"
 	"os"
+	"strings"
 )
 
 // NOTHING :  Not Found index
 const NOTHING int = -1
+const CHAR_NUMBER int = 128
 
 func check(e error) {
 	if e != nil {
 		panic(e)
 	}
+}
+
+func normalize(text string) string {
+	runes := []rune(text)
+	for i := 0; i < len(runes); i++ {
+		if int(runes[i]) >= CHAR_NUMBER {
+			runes[i] = '0'
+		}
+	}
+	return string(runes)
 }
 
 func computeLongestSequence(pattern string, j int) int {
@@ -59,13 +71,15 @@ func getSequenceTable(pattern string) []int {
 
 // MatchString KMP func : find index in which pattern is found in text with KMP algorithm
 func MatchString(pattern, text string) int {
-	/*
-	   Args:
-	       text (string)   : text to be matched
-	       pattern(string) : pattern to be found in text
-	   Returns:
-	       found index (int) : index in which the pattern is found in text, value is -1 if not found
-	*/
+	/**
+	 *	Args:
+	 *    text (string)   : text to be matched
+	 *    pattern(string) : pattern to be found in text
+	 *Returns:
+	 *    found index (int) : index in which the pattern is found in text, value is -1 if not found
+	 */
+	pattern = strings.ToLower(pattern)
+	text = strings.ToLower(normalize(text))
 	found := -1
 	match := true
 	i := 0
@@ -96,22 +110,22 @@ func MatchString(pattern, text string) int {
 func readJSON(filename string) (string, string) {
 	type Input struct {
 		Keywords string
-		Text string
+		Text     string
 	}
-	var input Input;
+	var input Input
 
-	inputJSON, ioErr := ioutil.ReadFile(filename);
-	check(ioErr);
-	jsonErr := json.Unmarshal(inputJSON, &input);
+	inputJSON, ioErr := ioutil.ReadFile(filename)
+	check(ioErr)
+	jsonErr := json.Unmarshal(inputJSON, &input)
 	if jsonErr == nil {
-		return input.Keywords, input.Text;
+		return input.Keywords, input.Text
 	} else {
-		return "", "";
+		return "", ""
 	}
 }
 
 func main() {
-	keywords, text := readJSON(os.Args[1]);
+	keywords, text := readJSON(os.Args[1])
 	idx := MatchString(string(keywords), string(text))
 	if idx != -1 {
 		fmt.Println(idx)
--- src/boyer-moore/boyer-moore.go
+++ src/boyer-moore/boyer-moore.go
@@ -1,109 +1,109 @@
 package main
 
 import (
-	"fmt"
-	"strings"
-	"io/ioutil"
 	"encoding/json"
+	"fmt"
+	"io/ioutil"
 	"os"
+	"strings"
 )
 
-const CHAR_NUMBER int = 128;
-const NOTHING int = -1;
+const CHAR_NUMBER int = 128
+const NOTHING int = -1
 
 func check(e error) {
 	if e != nil {
-		panic(e);
+		panic(e)
 	}
 }
 
 func normalize(text string) string {
-	runes := []rune(text);
+	runes := []rune(text)
 	for i := 0; i < len(runes); i++ {
-		if (int(runes[i]) >= CHAR_NUMBER) {
-			runes[i] = '0';
+		if int(runes[i]) >= CHAR_NUMBER {
+			runes[i] = '0'
 		}
 	}
-	return string(runes);
+	return string(runes)
 }
 
 func analyzeLastOccurence(keywords string) [CHAR_NUMBER]int {
-	var charIndices [CHAR_NUMBER] int;
+	var charIndices [CHAR_NUMBER]int
 
 	for i := 0; i < CHAR_NUMBER; i++ {
-		charIndices[i] = NOTHING;
+		charIndices[i] = NOTHING
 	}
 	for i := 0; i < len(keywords); i++ {
-		ascii := keywords[i];
-		charIndices[ascii] = i;
+		ascii := keywords[i]
+		charIndices[ascii] = i
 	}
 
-	return charIndices;
+	return charIndices
 }
 
 func solve(keywords, text string) int {
-	keywords = strings.ToLower(keywords);
-	text = strings.ToLower(normalize(text));
+	keywords = strings.ToLower(keywords)
+	text = strings.ToLower(normalize(text))
 
-	charIndices := analyzeLastOccurence(keywords);
+	charIndices := analyzeLastOccurence(keywords)
 
-	jMax := len(keywords) - 1;
-	iMax := len(text);
-	i := jMax;
-	j := 0;
-	found := -1;
+	jMax := len(keywords) - 1
+	iMax := len(text)
+	i := jMax
+	j := 0
+	found := -1
 
-	for (i < iMax && found == -1) {
-		j = 0;
-		for (j <= jMax && keywords[jMax - j] == text[i - j]) {
-			j++;
+	for i < iMax && found == -1 {
+		j = 0
+		for j <= jMax && keywords[jMax-j] == text[i-j] {
+			j++
 		}
-		if (j > jMax) {
-			found = i - (j - 1);
+		if j > jMax {
+			found = i - (j - 1)
 		} else {
-			indexInKeywords := jMax - j;
-			indexInText := i - j;
-			charInText := text[indexInText];
-			if (charIndices[charInText] == NOTHING) {
-				i += indexInKeywords + 1;
-			} else if (charIndices[charInText] > indexInKeywords) {
-				i++;
+			indexInKeywords := jMax - j
+			indexInText := i - j
+			charInText := text[indexInText]
+			if charIndices[charInText] == NOTHING {
+				i += indexInKeywords + 1
+			} else if charIndices[charInText] > indexInKeywords {
+				i++
 			} else {
-				i += indexInKeywords - charIndices[charInText];
+				i += indexInKeywords - charIndices[charInText]
 			}
 		}
 	}
 
-	return found;
+	return found
 }
 
 func readJSON(filename string) (string, string) {
 	type Input struct {
 		Keywords string
-		Text string
+		Text     string
 	}
-	var input Input;
+	var input Input
 
-	inputJSON, ioErr := ioutil.ReadFile(filename);
-	check(ioErr);
+	inputJSON, ioErr := ioutil.ReadFile(filename)
+	check(ioErr)
 
-	jsonErr := json.Unmarshal(inputJSON, &input);
+	jsonErr := json.Unmarshal(inputJSON, &input)
 	if jsonErr == nil {
-		return input.Keywords, input.Text;
+		return input.Keywords, input.Text
 	} else {
-		return "", "";
+		return "", ""
 	}
 }
 
 func main() {
-	keywords, text := readJSON(os.Args[1]);
-	fmt.Println(keywords);
-	fmt.Println(text);
-	idx := solve(keywords, text);
+	keywords, text := readJSON(os.Args[1])
+	fmt.Println(keywords)
+	fmt.Println(text)
+	idx := solve(keywords, text)
 
-	if (idx != -1) {
-		fmt.Println(text[idx:]);
+	if idx != -1 {
+		fmt.Println(text[idx:])
 	}
 
-	fmt.Println(idx);
+	fmt.Println(idx)
 }
